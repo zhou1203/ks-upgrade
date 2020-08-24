0856eb274445ef35258bd0f48b87465c0b869444
hongming <talonwan@yunify.com>
2020-08-24 07:32:29 -0500

    fix wrong placement after upgrade if multi-cluster is enabled (#13)
    Signed-off-by: hongming <talonwan@yunify.com>

--- pkg/workspaces/workspaces.go
+++ pkg/workspaces/workspaces.go
@@ -20,19 +20,21 @@ func NewWorkspaceMigrateTask(k8sClient kubernetes.Interface) task.UpgradeTask {
 }
 
 func (t *workspaceMigrateTask) Run() error {
-	cli := t.k8sClient.(*kubernetes.Clientset)
-	workspaces := make([]WorkspaceTemplate, 0)
-
-	data, err := cli.RESTClient().Get().AbsPath("/apis/tenant.kubesphere.io/v1alpha1/workspaces").DoRaw()
-
+	hostCluster, err := t.getHostCluster()
 	if err != nil {
 		klog.Error(err)
+		return err
+	}
+	cli := t.k8sClient.(*kubernetes.Clientset)
+	workspaces := make([]WorkspaceTemplate, 0)
+	data, err := cli.RESTClient().Get().AbsPath("/apis/tenant.kubesphere.io/v1alpha1/workspaces").DoRaw()
+	if err != nil {
+		klog.Error(err)
+		return err
 	}
 	var workspaceList WorkspaceList
 	json.Unmarshal(data, &workspaceList)
-
 	for _, workspace := range workspaceList.Items {
-
 		workspaceTemplate := WorkspaceTemplate{
 			TypeMeta: metav1.TypeMeta{
 				Kind:       "WorkspaceTemplate",
@@ -43,9 +45,20 @@ func (t *workspaceMigrateTask) Run() error {
 				Annotations: workspace.Annotations,
 			},
 			Spec: FederatedWorkspaceSpec{
-				Template:  workspace,
+				Template: Workspace{
+					ObjectMeta: metav1.ObjectMeta{
+						Name:        workspace.Name,
+						Labels:      workspace.Labels,
+						Annotations: workspace.Annotations,
+					},
+					Spec: workspace.Spec,
+				},
 				Placement: GenericPlacementFields{},
 			},
+		}
+
+		if hostCluster != "" {
+			workspaceTemplate.Spec.Placement.Clusters = []GenericClusterReference{{Name: hostCluster}}
 		}
 
 		if workspace.Name == "system-workspace" {
@@ -71,4 +84,23 @@ func (t *workspaceMigrateTask) Run() error {
 	}
 
 	return nil
+}
+
+func (t *workspaceMigrateTask) getHostCluster() (string, error) {
+	cli := t.k8sClient.(*kubernetes.Clientset)
+	data, err := cli.RESTClient().Get().AbsPath("/apis/cluster.kubesphere.io/v1alpha1/clusters").Param("labelSelector", "cluster-role.kubesphere.io/host").DoRaw()
+	if err != nil && !errors.IsNotFound(err) {
+		klog.Error(err)
+		return "", err
+	}
+	var list metav1.List
+	if err = json.Unmarshal(data, &list); err == nil && len(list.Items) > 0 {
+		var item struct {
+			Metadata metav1.ObjectMeta `json:"metadata"`
+		}
+		if err = json.Unmarshal(list.Items[0].Raw, &item); err == nil {
+			return item.Metadata.Name, nil
+		}
+	}
+	return "", nil
 }
