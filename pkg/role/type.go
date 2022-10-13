47ad11f06944229a07288d3ac618351183007987
Wenhao Zhou <wenhaozhou@yunify.com>
2022-10-13 18:46:29 +0800

    update vendor

--- pkg/role/type.go
+++ pkg/role/type.go
@@ -3,7 +3,6 @@ package role
 import (
 	rbacv1 "k8s.io/api/rbac/v1"
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
-	"kubesphere.io/ks-upgrade/pkg/globalrolebindings"
 )
 
 type WorkspaceRole struct {
@@ -25,5 +24,39 @@ type WorkspaceRoleList struct {
 type GlobalRoleList struct {
 	metav1.TypeMeta `json:",inline"`
 	metav1.ListMeta `json:"metadata,omitempty"`
-	Items           []globalrolebindings.GlobalRole `json:"items"`
+	Items           []GlobalRole `json:"items"`
+}
+
+type GlobalRoleBinding struct {
+	metav1.TypeMeta `json:",inline"`
+	// Standard object's metadata.
+	// +optional
+	metav1.ObjectMeta `json:"metadata,omitempty"`
+
+	// Subjects holds references to the objects the role applies to.
+	// +optional
+	Subjects []rbacv1.Subject `json:"subjects,omitempty" protobuf:"bytes,2,rep,name=subjects"`
+
+	// RoleRef can only reference a GlobalRole.
+	// If the RoleRef cannot be resolved, the Authorizer must return an error.
+	RoleRef rbacv1.RoleRef `json:"roleRef" protobuf:"bytes,3,opt,name=roleRef"`
+}
+
+type GlobalRoleBindingList struct {
+	metav1.TypeMeta `json:",inline"`
+	// Standard object's metadata.
+	// +optional
+	metav1.ListMeta `json:"metadata,omitempty"`
+	Items           []GlobalRoleBinding `json:"items"`
+}
+
+type GlobalRole struct {
+	metav1.TypeMeta `json:",inline"`
+	// Standard object's metadata.
+	// +optional
+	metav1.ObjectMeta `json:"metadata,omitempty"`
+
+	// Rules holds all the PolicyRules for this GlobalRole
+	// +optional
+	Rules []rbacv1.PolicyRule `json:"rules" protobuf:"bytes,2,rep,name=rules"`
 }
